6b12f879403e8a3255507f8c57e5b9c64cd6fcf4
Francis Levesque <flevesqu@cisco.com>
2020-09-08 19:45:12 -0600

    Adding a new activity

---
+++ app/src/main/java/com/francislevesque/lightitup/GameActivity.kt
@@ -0,0 +1,11 @@
+package com.francislevesque.lightitup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
